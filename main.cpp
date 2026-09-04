a8d7509a1e2f1517cc568470f29cf612547866df
rlatldbs1234 <rlatldbs1234@ewha.ac.kr>
2026-09-04 01:23:13 +0000

    message

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main()
 {
     int num0fStudents{31};
     std::cout << num0fStudents << std::end1;
-
+    std::cout << sizeof(num0fStudents)<< std::end1;
+    std::cout << typeid(num0fStudents).name<< std::end1;
     return 0;
 }
